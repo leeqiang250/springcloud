d1d794e3a8ebd14445e40c8c868d19ba1cdacef4
leeqiang250 <leeqiang250@163.com>
2019-07-10 16:12:32 +0800

    BUG修复。

--- eureka-client/src/main/java/cn/cloud/eurekaclient/test/TestController.java
+++ eureka-client/src/main/java/cn/cloud/eurekaclient/test/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("test")
 public class TestController {
-	
+
 	@GetMapping("test")
 	public JSONObject getNewCode() {
 		return new JSONObject();
