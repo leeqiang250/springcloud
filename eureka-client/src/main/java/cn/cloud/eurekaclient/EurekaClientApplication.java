2cdca07f2b9e6a7ec09aaa1ebb59d40e8ab9ee65
leeqiang250 <leeqiang250@163.com>
2019-07-10 15:57:58 +0800

    BUG修复。

--- eureka-client/src/main/java/cn/cloud/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/cn/cloud/eurekaclient/EurekaClientApplication.java
@@ -2,8 +2,12 @@ package cn.cloud.eurekaclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
 public class EurekaClientApplication {
 
 	public static void main(String[] args) {
---
+++ eureka-client/src/main/java/cn/cloud/eurekaclient/test/TestController.java
@@ -0,0 +1,17 @@
+package cn.cloud.eurekaclient.test;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+	
+	@GetMapping("test")
+	public JSONObject getNewCode() {
+		return new JSONObject();
+	}
+
+}
