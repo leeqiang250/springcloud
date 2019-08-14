6e46e41d68dda455a33f874f62cee21c446ed4d7
leeqiang250 <leeqiang250@163.com>
2019-08-14 18:48:56 +0800

    BUG修复。

---
+++ service-unit/src/main/java/cn/cloud/serviceunit/ServiceUnitApplication.java
@@ -0,0 +1,13 @@
+package cn.cloud.serviceunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceUnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceUnitApplication.class, args);
+	}
+
+}
---
+++ service-unit/src/test/java/cn/cloud/serviceunit/ServiceUnitApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.cloud.serviceunit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceUnitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
